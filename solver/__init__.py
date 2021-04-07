4d659fa26619a6d346fea3ad68ac6af882f2878b
thibsej <thibault.sejourne@ens.fr>
2021-04-07 10:29:16 +0200

    Update solver __init__

--- solver/__init__.py
+++ solver/__init__.py
@@ -4,7 +4,6 @@ All the code on functionals derived fron the theory of entropically regularized 
 
 __version__ = 0.1
 
-from .utils_numpy import euclid_dist
-from .utils_pytorch import l2_distortion, grad_l2_distortion, euclid_dist
-from .tlb_kl_sinkhorn_solver import TLBSinkhornSolver
+from .utils import euclid_dist, generate_measure
 from .batch_sinkhorn_solver import BatchSinkhornSolver
+from .vanilla_sinkhorn_solver import VanillaSinkhornSolver
