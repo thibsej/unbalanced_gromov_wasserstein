e715063480897b19012b6ef1b4711ada96e91ad1
François-Xavier VIALARD <vialard@ceremade.dauphine.fr>
2021-04-09 09:49:31 +0200

    "test fx"

---
+++ examples/test_fx.py
@@ -0,0 +1,62 @@
+import torch
+from solver.vanilla_sinkhorn_solver import VanillaSinkhornSolver
+from solver.utils import generate_measure
+torch.set_printoptions(8)
+import itertools
+
+def create_plan(permutation):
+    n = len(permutation)
+    pi = torch.zeros(n,n)
+    for i in range(n):
+        pi[i,permutation[i]] = 1.
+    return pi
+
+
+
+
+# Set up a solver for KL-(U)GW
+# Set rho=None to run balanced GW computation
+solv = VanillaSinkhornSolver(nits_plan=1000, nits_sinkhorn=1000, gradient=False, tol_plan=1e-5, tol_sinkhorn=1e-5,
+                             eps=0.1, rho=None)
+
+# Generate two mm-spaces with euclidean metrics
+a, Cx, _ = generate_measure(n_batch=1, n_sample=3, n_dim=3)
+b, Cy, _ = generate_measure(n_batch=1, n_sample=3, n_dim=2)
+a, b, Cx, Cy = a[0], b[0], Cx[0], Cy[0] # Reduce the first axis to keep one space (method generates batches)
+
+# Compute the bi-convex relaxation of the UGW problem
+pi, gamma = solv.alternate_sinkhorn(a, Cx, b, Cy)
+
+
+
+# Compute the loss
+cost = solv.ugw_cost(pi, gamma, a, Cx, b, Cy)
+print("Cost of the biconvex relaxation: ", cost)
+
+# Check the bi-convex relaxations is tight (otherwise the output is a local minima)
+cost_pi = solv.ugw_cost(pi, pi, a, Cx, b, Cy)
+cost_gamma = solv.ugw_cost(gamma, gamma, a, Cx, b, Cy)
+print("UGW cost with twice the same inputs for pi / gamma: ", (cost_pi, cost_gamma))
+
+# Switch to solving Balanced-GW
+solv.rho = None
+
+# Compute the loss and check the biconvex relaxation
+pi, gamma = solv.alternate_sinkhorn(a, Cx, b, Cy)
+cost = solv.ugw_cost(pi, gamma, a, Cx, b, Cy)
+print("GW cost of the biconvex relaxation: ", cost)
+cost_pi = solv.ugw_cost(pi, pi, a, Cx, b, Cy)
+cost_gamma = solv.ugw_cost(gamma, gamma, a, Cx, b, Cy)
+cost_gamma = solv.l2_distortion(gamma, gamma, Cx, Cy)
+print("GW cost with twice the same inputs for pi / gamma: ", (cost_pi, cost_gamma))
+
+temp = 1000000
+for perm in itertools.permutations(range(3),3):
+    pi_ = create_plan(perm)
+    cost = solv.l2_distortion(pi_, pi_, Cx, Cy)
+    #print(cost)
+    if cost < temp:
+        temp = cost
+        plan = pi_
+print(plan)
+print(cost)
