b546d46efe370fc08b4715945bcba50677b231ee
François-Xavier VIALARD <vialard@ceremade.dauphine.fr>
2021-04-09 13:22:36 +0200

    "permutation test in test_fx.py"

--- examples/test_fx.py
+++ examples/test_fx.py
@@ -3,7 +3,7 @@ from solver.vanilla_sinkhorn_solver import VanillaSinkhornSolver
 from solver.utils import generate_measure
 torch.set_printoptions(8)
 import itertools
-
+import pylab as pl
 ## Helper that construct a plan from a permutation.
 def create_plan(permutation):
     n = len(permutation)
@@ -18,16 +18,15 @@ def create_plan(permutation):
 # Set up a solver for KL-(U)GW
 # Set rho=None to run balanced GW computation
 solv = VanillaSinkhornSolver(nits_plan=1000, nits_sinkhorn=1000, gradient=False, tol_plan=1e-5, tol_sinkhorn=1e-5,
-                             eps=0.005, rho=None)
+                             eps=0.1, rho=None)
 
 n_sample = 3
 # Generate two mm-spaces with euclidean metrics
-a, Cx, _ = generate_measure(n_batch=1, n_sample=n_sample, n_dim=3,equal=True)
-b, Cy, _ = generate_measure(n_batch=1, n_sample=n_sample, n_dim=2,equal=True)
+a, Cx, x_a = generate_measure(n_batch=1, n_sample=n_sample, n_dim=2,equal=True)
+b, Cy, x_b = generate_measure(n_batch=1, n_sample=n_sample, n_dim=2,equal=True)
 a, b, Cx, Cy = a[0], b[0], Cx[0], Cy[0] # Reduce the first axis to keep one space (method generates batches)
 
 # Compute the bi-convex relaxation of the UGW problem
-pi, gamma = solv.alternate_sinkhorn(a, Cx, b, Cy)
 
 
 
@@ -50,5 +49,11 @@ for perm in itertools.permutations(range(n_sample),n_sample):
     if cost < temp:
         temp = cost
         plan = pi_
-#print(plan)
-print("best cost on permutations: ",cost)
+
+print("best cost on permutations: ",temp)
+
+print(plan)
+print(pi)
+pl.plot(x_a[0,:,0],x_a[0,:,1],"bo")
+pl.plot(x_b[0,:,0],x_b[0,:,1],"ro")
+pl.show()
