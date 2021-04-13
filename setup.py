af4682d3b589ea4832074e6e3c499c8652bc7007
thibsej <thibault.sejourne@ens.fr>
2021-04-13 12:35:58 +0200

    Update init and add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="unbalanced_gromov_wasserstein",
+    version="0.1.0",
+    description="Computation of Unbalanced Gromov-Wasserstein distances using entropic regularization",
+    author='Thibault Sejourne',
+    author_email='thibault.sejourne@ens.fr',
+    url='https://github.com/thibsej/unbalanced_gromov_wasserstein',
+    packages=find_packages(),
+    license="MIT",
+)
